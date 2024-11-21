43f595a6ec2f54de9f52330814844f6ebcbe7490
Harman Dhiman <harmandhimn6776@gmail.com>
2024-11-21 21:18:58 +0530

    fixing

--- Backend-JOURNIO/routes/paymentRoutes.js
+++ Backend-JOURNIO/routes/paymentRoutes.js
@@ -2,10 +2,8 @@ const express = require('express');
 const { createPaymentIntent, handleWebhook } = require('../controllers/PaymentController');
 const router = express.Router();
 
-// Route to create payment intent
 router.post('/create-payment-intent', createPaymentIntent);
 
-// Stripe webhook endpoint
 router.post('/webhook', express.raw({ type: 'application/json' }), handleWebhook);
 
 module.exports = router;
