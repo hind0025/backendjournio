abbf5ba223f363872a60e6eb84a6da5a4bd559ee
hind0025 <s.hindveer2004@gmail.com>
2024-11-22 20:46:53 +0530

    rating mongodob integration

--- Backend-JOURNIO/routes/Ratingroute.js
+++ Backend-JOURNIO/routes/Ratingroute.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const { submitRating, getServiceRatings } = require('../controllers/ratingController');
 const router = express.Router();
+const ratingform=require('../models/Rating')
 
 // Submit rating and feedback
 router.post('/submit', submitRating);
 
 // Get ratings for a service
 router.get('/:serviceId', getServiceRatings);
+//fecth all rating form
+router.get('/list', async (req, res) => {
+    try {
+        const ratings = await ratingform.find();
+        res.status(200).json(ratings);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching ratings', error });
+    }
+});
 
 module.exports = router;
